55caed6ef43bef8135fd4453cf1fdad2387ea16a
BennerLukas <arbeit.protonmail.com>
2022-07-14 14:30:20 +0200

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# rl-cpu-v-gpu
-Show and test differences of a Reinforcment Learning training with GPU or only CPU
+# Reinforcment Learning: Performance Differences
+
+When to use a CPU and when a GPU for reinforcment or other deep learning algorithms? The following will discuss the benefits and drawbacks of different approaches.
+
+Find more info on the [blogpost](medium.com).
---
+++ src/dqn.py
@@ -0,0 +1,44 @@
+import ray  # install tensorflow beforehand manually
+# install CUDA manually - for GPU support
+
+from ray import tune
+import os
+
+ray.init(ignore_reinit_error=True)
+log_dir = "log_run"
+log_path = os.path.join(os.getcwd(), log_dir)
+
+
+# run without GPU
+print("Start RL with GPU ")
+tune.run("DQN",
+         config={
+             "env": "CartPole-v1",
+             "num_gpus": 1,  # with gpu
+             "seed": 123,
+             # "evaluation_interval": 2,
+             # "evaluation_duration": 10
+         },
+         local_dir=log_dir,
+         name="with_GPU",
+         stop=ray.tune.stopper.MaximumIterationStopper(10),
+         # time_budget_s=100
+         )
+
+
+# run without GPU
+print("Start RL without GPU ")
+tune.run("DQN",
+         config={
+             "env": "CartPole-v1",
+             "num_gpus": 0,  # no gpu
+             "seed": 123,
+             # "evaluation_interval": 2,
+             # "evaluation_duration": 10
+         },
+         local_dir=log_dir,
+         name="no_GPU",
+         stop=ray.tune.stopper.MaximumIterationStopper(10),
+         )
+
+pass
